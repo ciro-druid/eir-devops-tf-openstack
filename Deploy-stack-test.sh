77aa1930151b5efbb8f8347e60b94d5bc25cf661
root <root@ip-172-31-20-125.ec2.internal>
2023-09-18 22:55:11 +0000

    new updates

--- Deploy-stack-test.sh
+++ Deploy-stack-test.sh
@@ -5,6 +5,20 @@
 
 
 
+ for dir in */; do
+          if [[ -f "${dir}main.tf" ]]; then
+            echo "Deploying in directory: $dir"
+            cd "$dir"
+            terraform init \
+              -backend-config="bucket=${BUCKET_NAME}" \
+              -backend-config="key=tf-openstack/terraform.tfstate" \
+              -backend-config="region=us-east-1"
+            terraform plan -out=tfplan
+            terraform apply -auto-approve tfplan
+            cd ..
+          fi
+        done
+
 
 
 
