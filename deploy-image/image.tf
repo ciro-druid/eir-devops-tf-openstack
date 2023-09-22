a48b0dfc95b24f1b8d4f1eb18d861e38ffb248aa
root <root@ip-172-31-20-125.ec2.internal>
2023-09-22 12:20:20 +0000

    runner local IMAGE UPLOAD to Openstack

--- deploy-image/image.tf
+++ deploy-image/image.tf
@@ -13,3 +13,23 @@ resource "openstack_images_image_v2" "centos79" {
 #  member_id = "bed6b6cbb86a4e2d8dc2735c2f1000e4"
 #  status    = "accepted"
 #}
+resource "openstack_images_image_v2" "EIR-BE-IMG" {
+  name             = "Centos79"
+  local_file_path  = "/ftp-comviva/IMAGES/Comviva_L2_EIR-BE_CentOS_7.9.qcow2"
+
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+
+}
+
+resource "openstack_images_image_v2" "centos79" {
+  name             = "Centos79"
+  local_file_path  = "/ftp-comviva/IMAGES/Comviva_L2_IMDB_CentOS_7.9.qcow2"
+
+  container_format = "bare"
+  disk_format      = "qcow2"
+  visibility       = "public"
+
+}
+
