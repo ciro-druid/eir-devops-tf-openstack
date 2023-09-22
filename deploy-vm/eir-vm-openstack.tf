8b466d697d7dc5bde87535c1b2270ccf0885d7a3
root <root@ip-172-31-20-125.ec2.internal>
2023-09-22 15:08:57 +0000

    Fix Image VMs and networks

--- deploy-vm/eir-vm-openstack.tf
+++ deploy-vm/eir-vm-openstack.tf
@@ -1,36 +1,81 @@
+####  NETWORKs
+data "openstack_networking_network_v2" "eir-lab-cdr-net" {
+  name = "eir-lab-cdr-net"
+}
+
+data "openstack_networking_network_v2" "eir-lab-ssh-net" {
+  name = "eir-lab-ssh-net"
+}
+
+data "openstack_networking_network_v2" "eir-lab-sig-net" {
+  name = "eir-lab-sig-net"
+}
+
+
+data "openstack_networking_network_v2" "eir-lab-om-net" {
+  name = "eir-lab-om-net"
+}
+
+
+data "openstack_networking_network_v2" "eir-lab-bill-net" {
+  name = "eir-lab-bill-net"
+}
+
+data "openstack_networking_network_v2" "eir-lab-dbrep-net" {
+  name = "eir-lab-dbrep-net"
+}
+
+
+
+
+#### IMAGEs
 data "openstack_images_image_ids_v2" "eir_be_image" {
   name = "EIR-BE-IMG"
 }
 
+data "openstack_images_image_ids_v2" "imdb_image" {
+  name = "IMDB-IMG"
+}
+	
+
+
+
+
+
+##### VMs
 resource "openstack_compute_instance_v2" "EIR-BE" {
   name  = "EIR-BE-vm"
   count = 2
-  # image_id    = data.openstack_images_image_ids_v2.custom_image.id
   image_name = data.openstack_images_image_ids_v2.eir_be_image.name
-  #flavor_id  = "eir_be_flavor"
   flavor_name  = "eir_be_flavor"
-  #flavor_id  = "gen.nano"
   network {
-    uuid = "c705fd99-bc0f-460f-940f-6bdb9fd54e84"
+    #uuid = "c705fd99-bc0f-460f-940f-6bdb9fd54e84"
+    name = data.openstack_networking_network_v2.eir-lab-sig-net.name
+  }
+  network {
+    name = data.openstack_networking_network_v2.eir-lab-ssh-net.name
+  }
+  network {
+    name = data.openstack_networking_network_v2.eir-lab-om-net.name
   }
 }
 
 
 
-data "openstack_images_image_ids_v2" "imdb_image" {
-  name = "IMDB-IMG"
-}
-
 resource "openstack_compute_instance_v2" "IMDB" {
   name  = "IMDB-vm"
   count = 2
-  # image_id    = data.openstack_images_image_ids_v2.imdb_image.id
   image_name = data.openstack_images_image_ids_v2.imdb_image.name
-  #flavor_id  = "imdb_flavor"
   flavor_name  = "imdb_flavor"
-  #flavor_id  = "gen.nano"
   network {
-    uuid = "c705fd99-bc0f-460f-940f-6bdb9fd54e84"
+    #uuid = "c705fd99-bc0f-460f-940f-6bdb9fd54e84"
+    name = data.openstack_networking_network_v2.eir-lab-dbrep-net.name
+  }
+  network {
+    name = data.openstack_networking_network_v2.eir-lab-ssh-net.name
+  }
+  network {
+    name = data.openstack_networking_network_v2.eir-lab-om-net.name
   }
 }
 
